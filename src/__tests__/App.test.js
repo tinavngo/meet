2c210a56c3ad9c64522dd4d294f333171f88e38b
tinavngo <143037396+tinavngo@users.noreply.github.com>
2024-05-09 22:53:54 -0700

    Add EventList component

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,10 +1,21 @@
 import { render } from '@testing-library/react';
 import App from '../App';
+import CitySearch from '../components/CitySearch';
 
 describe('<App /> component', () => {
+    let AppDOM;
+    beforeEach(() => {
+        AppDOM = render(<App />).container.firstChild;
+    })
 
     test('renders list of events', () => {
         const AppDOM = render(<App />).container.firstChild;
         expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
+    });
+
+
+    test('render CitySearch', () => {
+        const AppDOM = render(<App />).container.firstChild;
+        expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
     });
 });
