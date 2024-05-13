4e35d6ab5d3e41290769c49431cb672b0e4b958a
tinavngo <143037396+tinavngo@users.noreply.github.com>
2024-05-13 16:33:42 -0700

    New component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import './App.css';
+import NumberOfEvents from './components/NumberOfEvents';
 
 const App = () => {
   return (
     <div className="App">
       <CitySearch />
       <EventList />
+      <NumberOfEvents />
     </div>
   );
 }
---
+++ src/components/NumberOfEvents.js
@@ -0,0 +1,7 @@
+const NumberOfEvents = () => {
+    return (
+        <div id="number-of-events"></div>
+    );
+}
+
+export default NumberOfEvents;
