60e99c28743935ee5bac0192a98f68973493895b
tinavngo <143037396+tinavngo@users.noreply.github.com>
2024-05-13 18:58:51 -0700

    Control event details
    Testing + implementation

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,6 +1,24 @@
-const Event = () => {
+import { useState } from "react";
+
+const Event = ({event}) => {
+  const [showDetails, setShowDetails] = useState(false);
     return (
-        <li></li>
+      <li className="event">
+        <h3>{event.summary}</h3>
+        <p>{event.created}</p>
+        <p>{event.location}</p>
+        <button
+        className="details-button"
+        onClick={() => setShowDetails(!showDetails)}>
+          {showDetails ? 'hide details' : 'show details'}
+        </button>
+        {showDetails ? (
+          <div className="details">
+            <h4>event details</h4>
+            <p>{event.description}</p>
+          </div>)
+        : null}
+      </li>
     );
 }
 
