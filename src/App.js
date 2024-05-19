3a68b74af69ac3a7343de478730434818c6abc67
tinavngo <143037396+tinavngo@users.noreply.github.com>
2024-05-19 00:21:21 -0700

    Corrected initial state of `loading` to `true`

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
   const [currentNOE, setCurrentNOE] = useState(32);
   const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
-  const [loading, setLoading] = useState('true');
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
 
   const fetchData = async () => {
