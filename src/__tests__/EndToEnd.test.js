9e0f6b7e9bd77a842dece0b6ae6b54aecd973cdb
tinavngo <143037396+tinavngo@users.noreply.github.com>
2024-05-29 16:03:24 -0700

    Update EndToEnd.test.js

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -5,11 +5,13 @@ describe('show/hide an event details', () => {
     let browser;
     let page;
     beforeAll(async () => {
-        browser = await puppeteer.launch({
+        browser = await puppeteer.launch(
+        /*{
             headless: false,
             slowMo: 250, // slow down by 250ms
             timeout: 0 // removes any puppeteer/browser timeout limitations (this isn't the same as the timeout of jest)
-        });
+        }*/
+    );
         page = await browser.newPage();
         await page.goto('http://localhost:3000/');
         await page.waitForSelector('.event');
